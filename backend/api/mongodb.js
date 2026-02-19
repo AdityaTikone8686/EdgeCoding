2211b4452b1c7c07010ef13d1868c4e200c79a03
AdityaTikone8686 <gigaminex@gmail.com>
2026-02-20 02:02:06 +0530

    Update mongodb.js

--- backend/api/mongodb.js
+++ backend/api/mongodb.js
@@ -6,6 +6,9 @@ let cachedDb = null;
 export async function connectToDB() {
   if (cachedClient && cachedDb) return { client: cachedClient, db: cachedDb };
 
+  if (!process.env.MONGODB_URI || !process.env.DB_NAME)
+    throw new Error("Missing MongoDB environment variables");
+
   const client = new MongoClient(process.env.MONGODB_URI);
   await client.connect();
   const db = client.db(process.env.DB_NAME);
