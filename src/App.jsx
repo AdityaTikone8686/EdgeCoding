392085e246f2fe6ff26a1479ea80dc83c0ba7ee1
AdityaTikone8686 <gigaminex@gmail.com>
2026-02-19 23:34:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,7 @@ export default function App() {
               }
             />
           </Routes>
+          <Route path="/register" element={<Register />} />
 
           {/* Footer */}
           <Footer />
