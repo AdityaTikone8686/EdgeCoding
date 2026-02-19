fb489e59081ce182c083797ed8c9e3b0eb12c3d1
AdityaTikone8686 <gigaminex@gmail.com>
2026-02-19 17:51:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthSystem from "./components/AuthSystem";
-import Dashboard from "./components/Dashboard"; // your dashboard
-import Home from "./components/Home";           // main page
+
+        
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<AuthSystem />} />
-        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
