757f69eb6ac86e4c7c848f8075c85e7b2e4c9187
AdityaTikone8686 <gigaminex@gmail.com>
2026-02-19 17:54:13 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthSystem from "./components/AuthSystem";
 
-        
-
 function App() {
   return (
     <BrowserRouter>
-      
+      <Routes>
+        <Route path="/" element={<AuthSystem />} />
+      </Routes>
     </BrowserRouter>
   );
 }
