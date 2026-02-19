e1be3c34295f3ad4c7cf1260906f2afee00487e6
AdityaTikone8686 <gigaminex@gmail.com>
2026-02-19 17:57:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AuthSystem from "./components/AuthSystem";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthSystem />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+)
